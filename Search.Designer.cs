98ad3cc14150c5d66f37c6d4ca6bcde54a894614
g4b0-88 <gfloresolea@gmail.com>
2020-09-23 10:15:49 -0500

    Agregar archivos de proyecto.

---
+++ Search.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace TableLookUp
+{
+    partial class SearchWindow
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchWindow));
+            this.dbTXT = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pwdTXT = new System.Windows.Forms.TextBox();
+            this.usrTXT = new System.Windows.Forms.TextBox();
+            this.ipTXT = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.loadingGif = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.loadingGif)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dbTXT
+            // 
+            this.dbTXT.Location = new System.Drawing.Point(96, 30);
+            this.dbTXT.Name = "dbTXT";
+            this.dbTXT.Size = new System.Drawing.Size(194, 20);
+            this.dbTXT.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Database:";
+            // 
+            // pwdTXT
+            // 
+            this.pwdTXT.Location = new System.Drawing.Point(96, 82);
+            this.pwdTXT.Name = "pwdTXT";
+            this.pwdTXT.PasswordChar = '•';
+            this.pwdTXT.Size = new System.Drawing.Size(162, 20);
+            this.pwdTXT.TabIndex = 4;
+            // 
+            // usrTXT
+            // 
+            this.usrTXT.Location = new System.Drawing.Point(96, 56);
+            this.usrTXT.Name = "usrTXT";
+            this.usrTXT.Size = new System.Drawing.Size(194, 20);
+            this.usrTXT.TabIndex = 3;
+            // 
+            // ipTXT
+            // 
+            this.ipTXT.Location = new System.Drawing.Point(96, 5);
+            this.ipTXT.Name = "ipTXT";
+            this.ipTXT.Size = new System.Drawing.Size(194, 20);
+            this.ipTXT.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Usr:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "IP / Hostname:";
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(264, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 18;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // loadingGif
+            // 
+            this.loadingGif.Image = ((System.Drawing.Image)(resources.GetObject("loadingGif.Image")));
+            this.loadingGif.Location = new System.Drawing.Point(12, 109);
+            this.loadingGif.Name = "loadingGif";
+            this.loadingGif.Size = new System.Drawing.Size(279, 34);
+            this.loadingGif.TabIndex = 21;
+            this.loadingGif.TabStop = false;
+            // 
+            // SearchWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 152);
+            this.Controls.Add(this.loadingGif);
+            this.Controls.Add(this.dbTXT);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pwdTXT);
+            this.Controls.Add(this.usrTXT);
+            this.Controls.Add(this.ipTXT);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "SearchWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Table Tree";
+            ((System.ComponentModel.ISupportInitialize)(this.loadingGif)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox dbTXT;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox pwdTXT;
+        private System.Windows.Forms.TextBox usrTXT;
+        private System.Windows.Forms.TextBox ipTXT;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox loadingGif;
+    }
+}
+
---
+++ Search.cs
@@ -0,0 +1,208 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+
+namespace TableLookUp
+{
+    public partial class SearchWindow : Form
+    {
+        private List<string> errores = new List<string>();
+        private List<string> globalTables = new List<string>();
+        private JArray allTables = new JArray();
+        private BackgroundWorker bgWorker;
+        private Stopwatch stopwatch;
+        private JSONWindow ex;
+        private ErrorWindow err;
+        private int totalTables = 0;
+        private int idFlag = 1;
+        public SearchWindow()
+        {
+            InitializeComponent();
+            loadingGif.SizeMode = PictureBoxSizeMode.StretchImage;
+            loadingGif.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UseWaitCursor = true;
+            button1.Enabled = false;
+            ipTXT.Enabled = false;
+            dbTXT.Enabled = false;
+            usrTXT.Enabled = false;
+            pwdTXT.Enabled = false;
+            loadingGif.Visible = true;
+
+            globalTables = new List<string>();
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            bgWorker = new BackgroundWorker();
+            bgWorker.DoWork += new DoWorkEventHandler(InitSearch);
+            bgWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(finished);
+            bgWorker.RunWorkerAsync();
+        }
+
+        private void InitSearch(object sender, DoWorkEventArgs e)
+        {
+            totalTables = 0;
+            try
+            {
+                allTables = rootTables(ipTXT.Text, dbTXT.Text, usrTXT.Text, pwdTXT.Text);
+                idFlag = 1;
+            }
+            catch (Exception ex)
+            {
+                idFlag = ex.HResult;
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                bgWorker.CancelAsync();
+            }
+        }
+
+        private void finished(object sender, RunWorkerCompletedEventArgs e)
+        {
+            loadingGif.Visible = false;
+            button1.Enabled = true;
+            ipTXT.Enabled = true;
+            dbTXT.Enabled = true;
+            usrTXT.Enabled = true;
+            pwdTXT.Enabled = true;
+            stopwatch.Stop();
+            this.UseWaitCursor = false;
+            if (idFlag == 1)
+            {
+                string time = stopwatch.Elapsed.ToString("hh\\:mm\\:ss\\.ffff");
+                ex = new JSONWindow(allTables, dbTXT.Text, totalTables, time);
+                ex.FormClosed += OnCloseJson;
+                ex.Show();
+                if (errores.Count > 0)
+                {
+                    err = new ErrorWindow(errores);
+                    err.Show();
+                }
+            }
+            bgWorker.Dispose();
+        }
+
+        public void OnCloseJson(object sender, EventArgs e)
+        {
+            string errMess = "";
+            try
+            {
+                if (ex != null && err != null)
+                {
+                    err.Close();
+                    err.Dispose();
+                }
+            }
+            catch (Exception exc)
+            {
+                errMess = exc.Message;
+            }
+        }
+
+        private JArray rootTables(string ip, string database, string usr, string pwd)
+        {
+            JArray tables = new JArray();
+            DataTable dt = new DataTable();
+            string query = @"select name from sysobjects where xtype = 'U' order by name asc";
+            string str = "server=" + ip + ";database=" + database + ";UID=" + usr + ";password=" + pwd;
+            SqlConnection con = new SqlConnection(str);
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            con.Close();
+            da.Dispose();
+            foreach(DataRow dr in dt.Rows)
+            {
+                DataTable parents = new DataTable();
+                string tableName = dr["name"].ToString().ToUpper();
+                query = @"SELECT
+                    count(distinct SOO.NAME) AS parent_table
+                  FROM sysforeignkeys FK
+                  LEFT JOIN SYSOBJECTS SO
+                    ON FK.fkeyid = SO.ID
+                  LEFT JOIN SYSOBJECTS SOO
+                    ON SOO.ID = FK.rkeyid
+                  LEFT JOIN SYSOBJECTS SOOO
+                    ON SOOO.ID = FK.constid
+                  LEFT JOIN information_schema.key_column_usage IE
+                    ON IE.CONSTRAINT_NAME = SOOO.NAME
+                    AND ie.ORDINAL_POSITION = FK.keyno
+                    AND ie.TABLE_NAME = SO.NAME
+                  WHERE SO.NAME = '" + tableName +@"'
+                  ORDER BY parent_table ASC";
+                con = new SqlConnection(str);
+                cmd = new SqlCommand(query, con);
+                con.Open();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(parents);
+                con.Close();
+                da.Dispose();
+                int totalParents = Convert.ToInt32(parents.Rows[0]["parent_table"].ToString());
+                if(totalParents == 0)
+                {
+                    totalTables++;
+                    JObject obj = new JObject {{ "name", tableName}};
+                    JArray subChilds = childFromParent(tableName, str);
+                    if(subChilds.Count > 0)
+                    {
+                        obj.Add("related", subChilds);
+                    }
+                    tables.Add(obj);
+                }
+            }
+            return tables;
+        }
+
+        private JArray childFromParent(string tableName, string conStr)
+        {
+            globalTables.Add(tableName);
+            JArray tables = new JArray();
+            DataTable dt = new DataTable();
+            string query = @" SELECT
+                distinct SO.NAME AS foreign_table
+                FROM sysforeignkeys FK
+                LEFT JOIN SYSOBJECTS SO
+                ON FK.fkeyid = SO.ID
+                LEFT JOIN SYSOBJECTS SOO
+                ON SOO.ID = FK.rkeyid
+                LEFT JOIN SYSOBJECTS SOOO
+                ON SOOO.ID = FK.constid
+                LEFT JOIN information_schema.key_column_usage IE
+                ON IE.CONSTRAINT_NAME = SOOO.NAME
+                AND IE.ORDINAL_POSITION = FK.keyno
+                AND IE.TABLE_NAME = SO.NAME
+                WHERE SOO.NAME = '" + tableName + @"'
+                ORDER BY foreign_table ASC";
+            SqlConnection con = new SqlConnection(conStr);
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            con.Close();
+            da.Dispose();
+            foreach (DataRow dr in dt.Rows)
+            {
+                string childName = dr["foreign_table"].ToString().ToUpper();
+                if (!globalTables.Contains(childName))
+                {
+                    JObject child = new JObject { { "name", childName } };
+                    totalTables++;
+                    JArray subChilds = childFromParent(childName, conStr);
+                    if (subChilds.Count > 0)
+                    { child.Add("related", subChilds); }
+                    tables.Add(child);
+                }
+            }
+            return tables;
+        }
+    }
+}
